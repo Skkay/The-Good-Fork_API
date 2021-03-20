2fea8c17533e3123909567550ea1dc4018fcd05e
Skkay <skkay.thomas@gmail.com>
2021-03-20 19:09:00 +0100

    Test: testing authorization on '/drinks' endpoint

--- tests/AuthorizationTest.php
+++ tests/AuthorizationTest.php
@@ -52,4 +52,92 @@ class AuthorizationTest extends ApiTestCase
         $client->request('GET', $this->api_prefix_url.'/users', ['auth_bearer' => $tokenAdminUser]);
         $this->assertResponseStatusCodeSame(200);
     }
+
+    /**
+     * @depends App\Tests\AuthenticationTest::testLogin
+     */
+    public function testAuthorizationForDrinks(): void
+    {
+        $client = self::createClient();
+
+        // Retrieve a token for STANDARD USER
+        $response = $client->request('POST', $this->api_prefix_url.'/login', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'email' => "test_user@example.com",
+                'password' => "a_weak_user_password"
+            ]
+        ]);
+        $tokenStandardUser = $response->toArray()['token'];
+
+        // Retrieve a token for ADMIN USER
+        $response = $client->request('POST', $this->api_prefix_url.'/login', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'email' => "test_admin@example.com",
+                'password' => "a_strong_admin_password"
+            ]
+        ]);
+        $tokenAdminUser = $response->toArray()['token'];
+
+
+        // Tests for non authenticate user
+        $client->request('GET', $this->api_prefix_url.'/drinks');
+        $this->assertResponseStatusCodeSame(401);
+
+        $client->request('POST', $this->api_prefix_url.'/drinks', ['headers' => ['Content-Type' => 'application/json'], 'json' => ['name' => "New Drink"]]);
+        $this->assertResponseStatusCodeSame(401);
+
+        $client->request('GET', $this->api_prefix_url.'/drinks/1');
+        $this->assertResponseStatusCodeSame(401);
+
+        $client->request('PUT', $this->api_prefix_url.'/drinks/1', ['headers' => ['Content-Type' => 'application/json'], 'json' => ['name' => "Replaced Drink"]]);
+        $this->assertResponseStatusCodeSame(401);
+
+        $client->request('PATCH', $this->api_prefix_url.'/drinks/1', ['headers' => ['Content-Type' => 'application/merge-patch+json'], 'json' => ['name' => "Updated Drink"]]);
+        $this->assertResponseStatusCodeSame(401);
+
+        $client->request('DELETE', $this->api_prefix_url.'/drinks/1');
+        $this->assertResponseStatusCodeSame(401);
+
+
+        // Test for authenticate STANDARD USER
+        $client->request('GET', $this->api_prefix_url.'/drinks', ['auth_bearer' => $tokenStandardUser]);
+        $this->assertResponseStatusCodeSame(200);
+
+        $client->request('POST', $this->api_prefix_url.'/drinks', ['auth_bearer' => $tokenStandardUser, 'headers' => ['Content-Type' => 'application/json'], 'json' => ['name' => "New Drink"]]);
+        $this->assertResponseStatusCodeSame(403);
+
+        $client->request('GET', $this->api_prefix_url.'/drinks/1', ['auth_bearer' => $tokenStandardUser]);
+        $this->assertResponseStatusCodeSame(200);
+
+        $client->request('PUT', $this->api_prefix_url.'/drinks/1', ['auth_bearer' => $tokenStandardUser, 'headers' => ['Content-Type' => 'application/json'], 'json' => ['name' => "Replaced Drink"]]);
+        $this->assertResponseStatusCodeSame(403);
+
+        $client->request('PATCH', $this->api_prefix_url.'/drinks/1', ['auth_bearer' => $tokenStandardUser, 'headers' => ['Content-Type' => 'application/merge-patch+json'], 'json' => ['name' => "Updated Drink"]]);
+        $this->assertResponseStatusCodeSame(403);
+
+        $client->request('DELETE', $this->api_prefix_url.'/drinks/1', ['auth_bearer' => $tokenStandardUser]);
+        $this->assertResponseStatusCodeSame(403);
+
+
+        // Test for authenticate ADMIN USER
+        $client->request('GET', $this->api_prefix_url.'/drinks', ['auth_bearer' => $tokenAdminUser]);
+        $this->assertResponseStatusCodeSame(200);
+
+        $client->request('POST', $this->api_prefix_url.'/drinks', ['auth_bearer' => $tokenAdminUser, 'headers' => ['Content-Type' => 'application/json'], 'json' => ['name' => "New Drink"]]);
+        $this->assertResponseStatusCodeSame(201);
+
+        $client->request('GET', $this->api_prefix_url.'/drinks/1', ['auth_bearer' => $tokenAdminUser]);
+        $this->assertResponseStatusCodeSame(200);
+
+        $client->request('PUT', $this->api_prefix_url.'/drinks/1', ['auth_bearer' => $tokenAdminUser, 'headers' => ['Content-Type' => 'application/json'], 'json' => ['name' => "Replaced Drink"]]);
+        $this->assertResponseStatusCodeSame(200);
+
+        $client->request('PATCH', $this->api_prefix_url.'/drinks/1', ['auth_bearer' => $tokenAdminUser, 'headers' => ['Content-Type' => 'application/merge-patch+json'], 'json' => ['name' => "Updated Drink"]]);
+        $this->assertResponseStatusCodeSame(200);
+
+        $client->request('DELETE', $this->api_prefix_url.'/drinks/1', ['auth_bearer' => $tokenAdminUser]);
+        $this->assertResponseStatusCodeSame(204);
+    }
 }
