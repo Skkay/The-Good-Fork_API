7472937d15b08d5badfc477a4f22398505f725d4
Skkay <skkay.thomas@gmail.com>
2021-03-24 23:48:40 +0100

    Throw 'EntityNotFoundException' if 'drink' or 'food' is not found
    Try to find the drink or food passed to $data. Throw exception if it's not found, otherwise replace by found drink or food

--- src/DataPersister/MenuDataPersister.php
+++ src/DataPersister/MenuDataPersister.php
@@ -2,8 +2,11 @@
 
 namespace App\DataPersister;
 
+use App\Entity\Food;
 use App\Entity\Menu;
+use App\Entity\Drink;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Exception\EntityNotFoundException;
 use App\Exception\FieldAlreadyUsedException;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
@@ -24,6 +27,30 @@ class MenuDataPersister implements ContextAwareDataPersisterInterface
 
     public function persist($data, array $context = [])
     {
+        // Try to find the drink passed to $data. Throw exception if it's not found, otherwise replace by found drink
+        $drinkRepository = $this->em->getRepository(Drink::class);
+        foreach($data->getDrinks() as $requestedDrink) {
+            $drink = $drinkRepository->findOneByName($requestedDrink->getName());
+            if ($drink === null) {
+                throw new EntityNotFoundException(sprintf('Drink "%s" not found', $requestedDrink->getName()));
+            }
+
+            $data->removeDrink($requestedDrink);
+            $data->addDrink($drink);
+        }
+
+        // Try to find the food passed to $data. Throw exception if it's not found, otherwise replace by found food
+        $foodRepository = $this->em->getRepository(Food::class);
+        foreach($data->getFoods() as $requestedFood) {
+            $food = $foodRepository->findOneByName($requestedFood->getName());
+            if ($food === null) {
+                throw new EntityNotFoundException(sprintf('Food "%s" not found', $requestedFood->getName()));
+            }
+
+            $data->removeFood($requestedFood);
+            $data->addFood($food);
+        }
+
         try {
             $this->em->persist($data);
             $this->em->flush();
