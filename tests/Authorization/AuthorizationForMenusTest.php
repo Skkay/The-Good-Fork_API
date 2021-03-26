26a6de75995d5198bd0376f5252ebc9feca45cd1
Skkay <skkay.thomas@gmail.com>
2021-03-26 19:55:19 +0100

    Refactor: reorganization of test files

---
+++ tests/Authorization/AuthorizationForMenusTest.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Tests\Authorization;
+
+use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+
+class AuthorizationForMenusTest extends ApiTestCase
+{
+    use ReloadDatabaseTrait;
+
+    private $api_prefix_url = '/api';
+
+    /**
+     * @depends App\Tests\Authentication\AuthenticationTest::testLogin
+     */
+    public function testAuthorizationForMenus(): void
+    {
+        $client = self::createClient();
+
+        // Retrieve a token for STANDARD USER
+        $response = $client->request('POST', $this->api_prefix_url.'/login', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'email' => "test_user@example.com",
+                'password' => "a_weak_user_password"
+            ]
+        ]);
+        $tokenStandardUser = $response->toArray()['token'];
+
+        // Retrieve a token for ADMIN USER
+        $response = $client->request('POST', $this->api_prefix_url.'/login', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'email' => "test_admin@example.com",
+                'password' => "a_strong_admin_password"
+            ]
+        ]);
+        $tokenAdminUser = $response->toArray()['token'];
+
+
+        // Tests for non authenticate user
+        $client->request('GET', $this->api_prefix_url.'/menus');
+        $this->assertResponseStatusCodeSame(401);
+
+        $client->request('POST', $this->api_prefix_url.'/menus', ['headers' => ['Content-Type' => 'application/json'], 'json' => ['name' => "New Menu"]]);
+        $this->assertResponseStatusCodeSame(401);
+
+        $client->request('GET', $this->api_prefix_url.'/menus/1');
+        $this->assertResponseStatusCodeSame(401);
+
+        $client->request('PUT', $this->api_prefix_url.'/menus/1', ['headers' => ['Content-Type' => 'application/json'], 'json' => ['name' => "Replaced Menu"]]);
+        $this->assertResponseStatusCodeSame(401);
+
+        $client->request('PATCH', $this->api_prefix_url.'/menus/1', ['headers' => ['Content-Type' => 'application/merge-patch+json'], 'json' => ['name' => "Updated Menu"]]);
+        $this->assertResponseStatusCodeSame(401);
+
+        $client->request('DELETE', $this->api_prefix_url.'/menus/1');
+        $this->assertResponseStatusCodeSame(401);
+
+
+        // Test for authenticate STANDARD USER
+        $client->request('GET', $this->api_prefix_url.'/menus', ['auth_bearer' => $tokenStandardUser]);
+        $this->assertResponseStatusCodeSame(200);
+
+        $client->request('POST', $this->api_prefix_url.'/menus', ['auth_bearer' => $tokenStandardUser, 'headers' => ['Content-Type' => 'application/json'], 'json' => ['name' => "New Menu"]]);
+        $this->assertResponseStatusCodeSame(403);
+
+        $client->request('GET', $this->api_prefix_url.'/menus/1', ['auth_bearer' => $tokenStandardUser]);
+        $this->assertResponseStatusCodeSame(200);
+
+        $client->request('PUT', $this->api_prefix_url.'/menus/1', ['auth_bearer' => $tokenStandardUser, 'headers' => ['Content-Type' => 'application/json'], 'json' => ['name' => "Replaced Menu"]]);
+        $this->assertResponseStatusCodeSame(403);
+
+        $client->request('PATCH', $this->api_prefix_url.'/menus/1', ['auth_bearer' => $tokenStandardUser, 'headers' => ['Content-Type' => 'application/merge-patch+json'], 'json' => ['name' => "Updated Menu"]]);
+        $this->assertResponseStatusCodeSame(403);
+
+        $client->request('DELETE', $this->api_prefix_url.'/menus/1', ['auth_bearer' => $tokenStandardUser]);
+        $this->assertResponseStatusCodeSame(403);
+
+
+        // Test for authenticate ADMIN USER
+        $client->request('GET', $this->api_prefix_url.'/menus', ['auth_bearer' => $tokenAdminUser]);
+        $this->assertResponseStatusCodeSame(200);
+
+        // All should be good
+        $client->request('POST', $this->api_prefix_url.'/menus', ['auth_bearer' => $tokenAdminUser, 'headers' => ['Content-Type' => 'application/json'], 'json' => ['name' => "New Menu", 'price' => 10.99, 'drinks' => [['name' => 'Drink 1'], ['name' => 'Drink 2']], 'foods' => [['name' => 'Food 1'], ['name' => 'Food 2']]]]);
+        $this->assertResponseStatusCodeSame(201);
+
+        // Should throw exception for duplicate menu name
+        $client->request('POST', $this->api_prefix_url.'/menus', ['auth_bearer' => $tokenAdminUser, 'headers' => ['Content-Type' => 'application/json'], 'json' => ['name' => "New Menu", 'price' => 10.99, 'drinks' => [['name' => 'Drink 1'], ['name' => 'Drink 2']], 'foods' => [['name' => 'Food 1'], ['name' => 'Food 2']]]]);
+        $this->assertResponseStatusCodeSame(400);
+
+        // Should throw exception for unknown drink
+        $client->request('POST', $this->api_prefix_url.'/menus', ['auth_bearer' => $tokenAdminUser, 'headers' => ['Content-Type' => 'application/json'], 'json' => ['name' => "New Menu bis", 'price' => 10.99, 'drinks' => [['name' => 'Drink 1'], ['name' => 'Unknown Drink']], 'foods' => [['name' => 'Food 1'], ['name' => 'Food 2']]]]);
+        $this->assertResponseStatusCodeSame(400);
+
+        // Should throw exception for unknown food
+        $client->request('POST', $this->api_prefix_url.'/menus', ['auth_bearer' => $tokenAdminUser, 'headers' => ['Content-Type' => 'application/json'], 'json' => ['name' => "New Menu bis", 'price' => 10.99, 'drinks' => [['name' => 'Drink 1'], ['name' => 'Drink 2']], 'foods' => [['name' => 'Food 1'], ['name' => 'Unknown Food']]]]);
+        $this->assertResponseStatusCodeSame(400);
+
+        $client->request('GET', $this->api_prefix_url.'/menus/1', ['auth_bearer' => $tokenAdminUser]);
+        $this->assertResponseStatusCodeSame(200);
+
+        // All should be good
+        $client->request('PUT', $this->api_prefix_url.'/menus/1', ['auth_bearer' => $tokenAdminUser, 'headers' => ['Content-Type' => 'application/json'], 'json' => ['name' => "Replaced Menu", 'price' => 10.99, 'drinks' => [['name' => 'Drink 3'], ['name' => 'Drink 4']], 'foods' => [['name' => 'Food 4'], ['name' => 'Food 2']]]]);
+        $this->assertResponseStatusCodeSame(200);
+
+        // Should throw exception for unknown drink
+        $client->request('PUT', $this->api_prefix_url.'/menus/1', ['auth_bearer' => $tokenAdminUser, 'headers' => ['Content-Type' => 'application/json'], 'json' => ['name' => "Replaced Menu", 'price' => 10.99, 'drinks' => [['name' => 'Unknown Drink'], ['name' => 'Drink 4']], 'foods' => [['name' => 'Food 4'], ['name' => 'Food 2']]]]);
+        $this->assertResponseStatusCodeSame(400);
+
+        // Should throw exception for unknown food
+        $client->request('PUT', $this->api_prefix_url.'/menus/1', ['auth_bearer' => $tokenAdminUser, 'headers' => ['Content-Type' => 'application/json'], 'json' => ['name' => "Replaced Menu", 'price' => 10.99, 'drinks' => [['name' => 'Drink 3'], ['name' => 'Drink 4']], 'foods' => [['name' => 'Unknown Food'], ['name' => 'Food 2']]]]);
+        $this->assertResponseStatusCodeSame(400);
+
+        // All should be good
+        $client->request('PATCH', $this->api_prefix_url.'/menus/1', ['auth_bearer' => $tokenAdminUser, 'headers' => ['Content-Type' => 'application/merge-patch+json'], 'json' => ['name' => "Updated Menu"]]);
+        $this->assertResponseStatusCodeSame(200);
+
+        // Should throw exception for unknown drink
+        $client->request('PATCH', $this->api_prefix_url.'/menus/1', ['auth_bearer' => $tokenAdminUser, 'headers' => ['Content-Type' => 'application/merge-patch+json'], 'json' => ['drinks' => [['name' => 'Unknown Drink']]]]);
+        $this->assertResponseStatusCodeSame(400);
+
+        // Should throw exception for unknown food
+        $client->request('PATCH', $this->api_prefix_url.'/menus/1', ['auth_bearer' => $tokenAdminUser, 'headers' => ['Content-Type' => 'application/merge-patch+json'], 'json' => ['foods' => [['name' => 'Unknown Food']]]]);
+        $this->assertResponseStatusCodeSame(400);
+
+        $client->request('DELETE', $this->api_prefix_url.'/menus/1', ['auth_bearer' => $tokenAdminUser]);
+        $this->assertResponseStatusCodeSame(204);
+    }
+}
---
+++ tests/Authorization/AuthorizationForUsersTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Tests\Authorization;
+
+use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+
+class AuthorizationForUsersTest extends ApiTestCase
+{
+    use ReloadDatabaseTrait;
+
+    private $api_prefix_url = '/api';
+
+    /**
+     * @depends App\Tests\Authentication\AuthenticationTest::testLogin
+     */
+    public function testAuthorizationForUsers(): void
+    {
+        $client = self::createClient();
+
+        // Retrieve a token for STANDARD USER
+        $response = $client->request('POST', $this->api_prefix_url.'/login', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'email' => "test_user@example.com",
+                'password' => "a_weak_user_password"
+            ]
+        ]);
+        $tokenStandardUser = $response->toArray()['token'];
+
+        // Retrieve a token for ADMIN USER
+        $response = $client->request('POST', $this->api_prefix_url.'/login', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'email' => "test_admin@example.com",
+                'password' => "a_strong_admin_password"
+            ]
+        ]);
+        $tokenAdminUser = $response->toArray()['token'];
+
+        
+        // Test not authorized (without authentication)
+        $client->request('GET', $this->api_prefix_url.'/users');
+        $this->assertResponseStatusCodeSame(401);
+        
+        // Test not authorized (with authentication)
+        $client->request('GET', $this->api_prefix_url.'/users', ['auth_bearer' => $tokenStandardUser]);
+        $this->assertResponseStatusCodeSame(403);
+
+        // Test authorized (with authentication)
+        $client->request('GET', $this->api_prefix_url.'/users', ['auth_bearer' => $tokenAdminUser]);
+        $this->assertResponseStatusCodeSame(200);
+    }
+}
