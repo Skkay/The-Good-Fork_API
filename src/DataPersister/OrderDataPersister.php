7dbc713ee25bf262b4d54d314d3c9e4572c90365
Skkay <skkay.thomas@gmail.com>
2021-05-08 00:56:34 +0200

    Update 'OrderDataPersister' to record the quantiy of each item ordered

--- src/DataPersister/OrderDataPersister.php
+++ src/DataPersister/OrderDataPersister.php
@@ -6,7 +6,11 @@ use App\Entity\Food;
 use App\Entity\Menu;
 use App\Entity\Drink;
 use App\Entity\Order;
+use App\Entity\OrderFood;
+use App\Entity\OrderMenu;
+use App\Entity\OrderDrink;
 use App\Entity\OrderStatus;
+use Psr\Log\LoggerInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Exception\EntityNotFoundException;
 use Symfony\Component\Security\Core\Security;
@@ -16,11 +20,13 @@ class OrderDataPersister implements ContextAwareDataPersisterInterface
 {
     private $em;
     private $security;
+    private $logger;
 
-    public function __construct(EntityManagerInterface $em, Security $security)
+    public function __construct(EntityManagerInterface $em, Security $security, LoggerInterface $logger)
     {
         $this->em = $em;
         $this->security = $security;
+        $this->logger = $logger;
     }
 
     public function supports($data, array $context = []): bool
@@ -32,42 +38,60 @@ class OrderDataPersister implements ContextAwareDataPersisterInterface
     {
         $totalPrice = 0;
 
-        // Convert menu ids to menu object
+        // Convert menu ids to orderedMenu object with number of menus, then set it to data
         if (!empty($data->getMenuIds())) {
             $menuRepository = $this->em->getRepository(Menu::class);
-            foreach ($data->getMenuIds() as $menuId) {
+            $occurencies = array_count_values($data->getMenuIds());
+
+            foreach ($occurencies as $menuId => $quantity) {
                 $menu = $menuRepository->find($menuId);
                 if ($menu === null) {
                     throw new EntityNotFoundException(sprintf('Menu with id "%s" is not found', $menuId));
                 }
-                $data->addMenu($menu);
-                $totalPrice += $menu->getPrice();
+                $orderMenu = new OrderMenu();
+                $orderMenu->setMenu($menu);
+                $orderMenu->setQuantity($quantity);
+
+                $data->addOrderedMenu($orderMenu);
+                $totalPrice += $menu->getPrice() * $quantity;
             }
         }
         
-        // Convert food ids to food object
+        // Convert food ids to orderedFood object with number of foods, then set it to data
         if (!empty($data->getFoodIds())) {
             $foodRepository = $this->em->getRepository(Food::class);
-            foreach ($data->getFoodIds() as $foodId) {
+            $occurencies = array_count_values($data->getFoodIds());
+
+            foreach ($occurencies as $foodId => $quantity) {
                 $food = $foodRepository->find($foodId);
                 if ($food === null) {
                     throw new EntityNotFoundException(sprintf('Food with id "%s" is not found', $menuId));
                 }
-                $data->addFood($food);
-                $totalPrice += $food->getPrice();
+                $orderFood = new OrderFood();
+                $orderFood->setFood($food);
+                $orderFood->setQuantity($quantity);
+
+                $data->addOrderedFood($orderFood);
+                $totalPrice += $food->getPrice() * $quantity;
             }
         }
         
-        // Convert drink ids to drink object
+        // Convert drink ids to orderedDrink object with number of drinks, then set it to data       
         if (!empty($data->getDrinkIds())) {
             $drinkRepository = $this->em->getRepository(Drink::class);
-            foreach ($data->getDrinkIds() as $drinkId) {
+            $occurencies = array_count_values($data->getDrinkIds());
+
+            foreach ($occurencies as $drinkId => $quantity) {
                 $drink = $drinkRepository->find($drinkId);
                 if ($drink === null) {
                     throw new EntityNotFoundException(sprintf('Drink with id "%s" is not found', $menuId));
                 }
-                $data->addDrink($drink);
-                $totalPrice += $drink->getPrice();
+                $orderDrink = new OrderDrink();
+                $orderDrink->setDrink($drink);
+                $orderDrink->setQuantity($quantity);
+
+                $data->addOrderedDrink($orderDrink);
+                $totalPrice += $drink->getPrice() * $quantity;
             }
         }
 
